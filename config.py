4f1d40b27a82d660676f4901fc59111b41594f8f
tikaramgahane2k4 <gahanepawan07@gmail.com>
2025-12-27 00:15:45 +0530

    updated

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://postgres:user%40123@localhost:5432/student_ms_db")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATION = False
 SECRET_KEY = os.environ.get("SECRET_KEY", "student@123")
+
+# postgresql://student_ms_db_1i7f_user:oGpNiDNenVW5TjjLWOba66hcMtd3YqG0@dpg-d57c6peuk2gs73cvb8qg-a.oregon-postgres.render.com/student_ms_db_1i7f
